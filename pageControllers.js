88a43b322243b655455d43a53ab60dbf7bfb45fa
Mehmet Özkan <mehmetozkan@192.168.1.63>
2023-02-28 17:52:22 +0300

    denem

---
+++ pageControllers.js
@@ -0,0 +1,11 @@
+const Photo = require('../models/Photo')
+
+exports.getAboutPage = (req, res) => {
+  res.render('about');
+};
+
+exports.getAddPage = (req, res) => {
+  res.render('add');
+};
+
+};
